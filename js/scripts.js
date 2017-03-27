d3e756d87b4ce77f657db9afc9481dc44913f5db
Galin McMahon and Tyler Stephenson <tyler@>
2017-03-27 14:45:46 -0700

    create site using js to display cards with suits

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 $(function(){
+  var cards = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
+  var suits = ["Clubs", "Hearts", "Spades", "Diamonds"];
 
+  suits.forEach(function(suit){
+    cards.forEach(function(card){
+      $(`#${suit.toLowerCase()} ul`).append(`<li>${card} of ${suit}</li>`);
+    });
+  });
 
-  
 });
